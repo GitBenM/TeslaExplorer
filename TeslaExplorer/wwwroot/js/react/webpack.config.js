25693267ad0a1be1f48e86ade17ee49234019105
Benjamin Miller <GitBenM@users.noreply.github.com>
2019-07-09 21:34:22 -0400

    playing around with react

---
+++ TeslaExplorer/wwwroot/js/react/webpack.config.js
@@ -0,0 +1,40 @@
+﻿module.exports = {
+    mode: "production", // "production" | "development" | "none"
+    // Chosen mode tells webpack to use its built-in optimizations accordingly.
+    entry: { game: "./app.js", vehicle: "./vehicle.js" }, // string | object | array
+    // defaults to ./src
+    // Here the application starts executing
+    // and webpack starts bundling
+    context: __dirname,
+    output: {
+        // options related to how webpack emits results
+        path: __dirname + "/dist", // path.resolve(__dirname, "dist"), // string
+        // the target directory for all output files
+        // must be an absolute path (use the Node.js path module)
+        filename: "[name].js", // string
+        // the filename template for entry chunks
+        //publicPath: "/assets/", // string
+        // the url to the output directory resolved relative to the HTML page
+        //library: "tesla-explorer", // string,
+        // the name of the exported library
+        //libraryTarget: "umd", // universal module definition
+        // the type of the exported library
+        /* Advanced output configuration (click to show) */
+        /* Expert output configuration (on own risk) */
+    },
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    }
+}
--- TeslaExplorer/wwwroot/js/site.js
+++ TeslaExplorer/wwwroot/js/site.js
@@ -1,79 +1,79 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+//// for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+//// Write your JavaScript code.
 
-var vm = new Vue({
+//var vm = new Vue({
 
-});
+//});
 
-Vue.component('todo-item', {
-    props: ['todo'],
-    template: '<li>{{ todo.text }}</li>'
-});
+//Vue.component('todo-item', {
+//    props: ['todo'],
+//    template: '<li>{{ todo.text }}</li>'
+//});
 
-var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue'
-    }
-});
+//var app = new Vue({
+//    el: '#app',
+//    data: {
+//        message: 'Hello Vue'
+//    }
+//});
 
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-        message: 'You loaded this page on ' + new Date().toLocaleString()
-    }
-});
+//var app2 = new Vue({
+//    el: '#app-2',
+//    data: {
+//        message: 'You loaded this page on ' + new Date().toLocaleString()
+//    }
+//});
 
-var app3 = new Vue({
-    el: '#app-3',
-    data: {
-        seen: true
-    }
-});
+//var app3 = new Vue({
+//    el: '#app-3',
+//    data: {
+//        seen: true
+//    }
+//});
 
-var app4 = new Vue({
-    el: '#app-4',
-    data: {
-        todos: [
-            { text: 'Learn JavaScript' },
-            { text: 'Learn Vue' },
-            { text: 'Build something awesome' }
-        ]
-    }
-});
+//var app4 = new Vue({
+//    el: '#app-4',
+//    data: {
+//        todos: [
+//            { text: 'Learn JavaScript' },
+//            { text: 'Learn Vue' },
+//            { text: 'Build something awesome' }
+//        ]
+//    }
+//});
 
-var app5 = new Vue({
-    el: '#app-5',
-    data: {
-        message: 'Hello Vue.js'
-    },
-    methods:
-    {
-        reverseMessage: function () {
-            this.message = this.message.split('').reverse().join('');
-        }
-    }
-});
+//var app5 = new Vue({
+//    el: '#app-5',
+//    data: {
+//        message: 'Hello Vue.js'
+//    },
+//    methods:
+//    {
+//        reverseMessage: function () {
+//            this.message = this.message.split('').reverse().join('');
+//        }
+//    }
+//});
 
-var app6 = new Vue({
-    el: '#app-6',
-    data: {
-        message: 'Hello Vue'
-    }
-});
+//var app6 = new Vue({
+//    el: '#app-6',
+//    data: {
+//        message: 'Hello Vue'
+//    }
+//});
 
-var app7 = new Vue({
-    el: '#app-7',
-    data: {
-        groceryList: [
-            { id: 0, text: 'Veggies' },
-            { id: 1, text: 'Cheese' },
-            { id: 2, text: 'Some item'}
-        ]
-    }
-});
+//var app7 = new Vue({
+//    el: '#app-7',
+//    data: {
+//        groceryList: [
+//            { id: 0, text: 'Veggies' },
+//            { id: 1, text: 'Cheese' },
+//            { id: 2, text: 'Some item'}
+//        ]
+//    }
+//});
 
 
    
