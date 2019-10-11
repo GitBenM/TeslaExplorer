56a0ff046398930ef7e81bcb8c92092c23d9debc
Benjamin Miller <GitBenM@users.noreply.github.com>
2019-10-10 21:34:18 -0400

    fix .net core migration stuff

--- TeslaExplorer/Startup.cs
+++ TeslaExplorer/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 
 namespace TeslaExplorer
@@ -38,11 +39,11 @@ namespace TeslaExplorer
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
+            services.AddMvc(option => option.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Latest);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
